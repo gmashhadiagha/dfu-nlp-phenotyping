6bed087d9b9986f9a35408d352aa2ff82aaef212
gmashhadiagha <132790212+gmashhadiagha@users.noreply.github.com>
2026-05-22 13:51:27 -0400

    Diabetes_Cohort.sql

--- sql/01_icd_diabetes_cohort.sql
+++ sql/01_icd_diabetes_cohort.sql
@@ -1,1 +1,18 @@
 
+CREATE OR REPLACE TABLE `candidacy.dfu_project.dfu_cohort` AS
+SELECT DISTINCT
+  a.subject_id,
+  a.hadm_id,
+  a.admittime,
+  a.dischtime,
+  a.insurance,
+  d.icd_code
+FROM `physionet-data.mimiciv_3_1_hosp.admissions` a
+JOIN `physionet-data.mimiciv_3_1_hosp.diagnoses_icd` d
+  ON a.hadm_id = d.hadm_id
+WHERE
+  (
+    d.icd_code LIKE '250%' OR
+    d.icd_code LIKE 'E10%' OR
+    d.icd_code LIKE 'E11%'
+  );
